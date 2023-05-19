28648f6246860e862740cd7d337bcf39fbb08a2a
dungpqph36230 <dung11092004@gmail.com>
2023-05-19 14:04:40 +0700

    update

---
+++ app/src/main/java/com/example/lab3_ph36230/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lab3_ph36230
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linearlayout)
+    }
+}
